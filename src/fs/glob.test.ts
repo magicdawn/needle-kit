b44397d5dbdf397b0a2231533b08cf0768ba4c8b
magicdawn <magicdawn@qq.com>
2025-11-27 10:59:59 +0800

    fix(glob): fix matchFromList case issue

--- src/fs/glob.test.ts
+++ src/fs/glob.test.ts
@@ -13,5 +13,16 @@ describe('glob', () => {
       const matched = await matchFromList(fromList, './**/*.test.ts')
       expect(matched.includes(import.meta.filename)).toBe(true)
     })
+
+    it('case: default strict', async () => {
+      const fromList = [import.meta.dirname]
+      const matched = await matchFromList(fromList, '*.test.TS')
+      expect(matched.length).toBe(0)
+    })
+    it('case: insensitive works', async () => {
+      const fromList = [import.meta.dirname]
+      const matched = await matchFromList(fromList, '*.test.TS', { caseSensitiveMatch: false })
+      expect(matched.includes(import.meta.filename)).toBe(true)
+    })
   })
 })
--- src/fs/glob.ts
+++ src/fs/glob.ts
@@ -7,12 +7,13 @@ export function customFinderSort(files: string[]) {
   return finderSort(files, { locale: 'zh-CN' })
 }
 
-export async function matchFromList(selected: string[], pattern: string | string[], fgOptions?: Partial<fg.Options>) {
+export async function matchFromList(list: string[], pattern: string | string[], fgOptions?: Partial<fg.Options>) {
   const hasSlash = [pattern].flat().some((p) => p.includes('/'))
   const baseNameMatch = fgOptions?.baseNameMatch ?? !hasSlash
+  const caseSensitive = fgOptions?.caseSensitiveMatch ?? true // default strict
 
   const queue: string[] = []
-  for (const item of selected) {
+  for (const item of list) {
     if (await isSymlink(item)) {
       continue
     }
@@ -20,10 +21,10 @@ export async function matchFromList(selected: string[], pattern: string | string
     // dir
     if (await isDirectory(item)) {
       const files = await fg(pattern, {
-        caseSensitiveMatch: true,
         cwd: item,
         absolute: true,
         followSymbolicLinks: false,
+        caseSensitiveMatch: caseSensitive,
         baseNameMatch,
         ...fgOptions,
       })
@@ -32,7 +33,7 @@ export async function matchFromList(selected: string[], pattern: string | string
     // file
     else if (await isFile(item)) {
       // https://github.com/micromatch/micromatch/issues/9#issuecomment-1529612035
-      queue.push(...mm([item], pattern, { basename: baseNameMatch }))
+      queue.push(...mm([item], pattern, { basename: baseNameMatch, nocase: !caseSensitive }))
     }
   }
 
